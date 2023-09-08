1c20ee28b3c9f22ca1a2d4340ac08d5057be485e
Jiaxuan Yao <acu21jy@bessemer-login1.shef.ac.uk>
2023-09-08 21:58:13 +0100

    1

--- script/listen_l_m.sh
+++ script/listen_l_m.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time=24:00:00
 #SBATCH --mail-user=jyao28@sheffield.ac.uk
-#SBATCH --job-name=lst_l_m_0.3
+#SBATCH --job-name=lst_l_m_0.5
 #SBATCH --output=script/out/lst_l_m.%j.out
 
 module load Anaconda3/2019.07
@@ -15,4 +15,4 @@ module load FFmpeg/4.2.2-GCCcore-9.3.0
 
 # We assume that the conda environment 'myexperiment' has already been created
 source activate project
-python main.py -whisper l -model medium -path /fastdata/acu21jy -ratio 0.3 -level l
+python main.py -whisper l -model medium.en -path /fastdata/acu21jy -ratio 0.5 -level l
